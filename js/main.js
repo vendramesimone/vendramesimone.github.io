e30e9325f6e8cb19798420a03ad58246eeb772ff
vendramesimone <64363557+vendramesimone@users.noreply.github.com>
2023-03-14 14:33:45 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -32,9 +32,9 @@
             $('html, body').animate({ scrollTop: 0 }, 'normal');
 
             // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function() {
+            $("#loader").delay(5000).fadeOut("slow", function() {
                 // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(30000).fadeOut("slow");
+                $("#preloader").delay(300).fadeOut("slow");
             }); 
             
             // for hero content animations 
